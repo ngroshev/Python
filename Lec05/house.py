2cab494f5db693a8865a679f1f91b0a0d9cdfce3
ngroshev <nikita.groshev@gmail.com>
2020-11-25 04:01:30 +0000

    Three new prototype functions have been added.

--- Lec05/house.py
+++ Lec05/house.py
@@ -1,4 +1,12 @@
-def draw_house(x, y, witdth, height):
+def main ():
+    x, y = 300, 400
+    width, height = 200, 300
+
+    draw_house(x, y, width, height)
+
+
+
+def draw_house(x, y, width, height):
     """
     Function that draws house with full width and heght from the reper point (x, y),
     positioned in the bottom of the base.
@@ -8,12 +16,27 @@ def draw_house(x, y, witdth, height):
     :param height: full height
     :return:
     """
-    print ('Строим дом...', x, y, width, height)
-    pass # do nothing
+    print('Строим дом...', x, y, width, height)
+    foundation_height = 0.05 * height
+    walls_width = 0.9 * width
+    walls_height = 0.5 * height
+    roof_height = height - foundation_height - walls_height
 
-x, y = 100, 100
-width, height = 200, 300
+    draw_house_foundation (x, y, width, height)
+    draw_house_walls(x, y - foundation_height, walls_width, walls_height )
+    draw_house_roof(x, y - foundation_height - walls_height, width, roof_height)
 
-draw_house(x, y, width, height)
+
+def draw_house_foundation (x, y, width, height):
+    pass
 
 
+def draw_house_walls(x, y, width, height):
+    pass
+
+
+def draw_house_roof(x, y, width, height):
+    pass
+
+
+main()
