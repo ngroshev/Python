1e050a3fae32d5b9254b66226d5a8042767e7133
ngroshev <nikita.groshev@gmail.com>
2020-11-25 02:42:34 +0000

    Created function of drawing a house as a prototype
    Interface was documented

---
+++ Lec05/house.py
@@ -0,0 +1,19 @@
+def draw_house(x, y, witdth, height):
+    """
+    Function that draws house with full width and heght from the reper point (x, y),
+    positioned in the bottom of the base.
+    :param x: coordinate x of half of width
+    :param y: coordinate y of bottom of base
+    :param witdth: full width
+    :param height: full height
+    :return:
+    """
+    print ('Строим дом...', x, y, width, height)
+    pass # do nothing
+
+x, y = 100, 100
+width, height = 200, 300
+
+draw_house(x, y, width, height)
+
+
