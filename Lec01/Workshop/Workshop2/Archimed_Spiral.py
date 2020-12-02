ad806c35491e5a9957393285ce6910aed31a4344
ngroshev <nikita.groshev@gmail.com>
2020-12-02 03:37:14 +0000

    Function draws Aarhcimedes' spiral

---
+++ Lec01/Workshop/Workshop2/Archimed_Spiral.py
@@ -0,0 +1,23 @@
+import turtle
+import numpy as np
+
+
+def archimed_spiral(k,phi_rad):
+    t = turtle.Pen()
+    t.shape('turtle')
+    t.speed(0)
+    t.pensize(1)
+    phi_degr = phi_rad * (180 / np.pi)
+
+    for i in range (0,1000):
+        ro = k*phi_rad
+        #phi_degr = phi_rad*(180/np.pi)
+        t.forward(ro)
+        t.left(phi_degr)
+        phi_rad = phi_rad + 0.1
+
+def main():
+    archimed_spiral(1,0.3)
+
+
+main()
