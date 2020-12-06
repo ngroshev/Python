182ad68e36a74345fe0f5af9d1e50562c1781912
ngroshev <nikita.groshev@gmail.com>
2020-12-06 00:22:04 +0000

    latest version

--- Lec01/Workshop/Workshop1/Turtle_Concentr_Squares.py
+++ Lec01/Workshop/Workshop1/Turtle_Concentr_Squares.py
@@ -1,31 +1,30 @@
 import turtle
 
-
-def square(size):
+#def square(size):
     t = turtle.Pen()
     t.hideturtle()
     t.shape('turtle')
-    t.speed(3)
+    t.speed(1)
     t.pensize(2)
     t.penup()
-    t.goto(size, 0)
+    t.goto(size)
     t.pendown()
     t.left(90)
     t.forward(size)
-    t.left(90)
-    t.forward(2 * size)
-    t.left(90)
-    t.forward(2 * size)
-    t.left(90)
-    t.forward(2 * size)
-    t.left(90)
-    t.forward(size)
+    #t.left(90)
+    #t.forward(2*size)
+    #t.left(90)
+    #t.forward(2*size)
+    #t.left(90)
+   # t.forward(2*size)
+    #t.left(90)
+    #t.forward(size)
 
 
-def main():
-    size = 20
-    for step in range(10):
-        square(size * (1 + step))
+#def main():
+ #   size = 50
+ #   for step in range(10):
+  #      square(size * (1 + step))
+#
 
-
-main()
+#main()
